0b37d57117ed8ecd057bfd8780754d2298114e46
Name <abcdAgmail.com^C>
2024-03-03 21:16:21 +0530

    Add project files.

---
+++ CollegeApp1/Controllers/StudentController.cs
@@ -0,0 +1,168 @@
+﻿using CollegeApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CollegeApp1.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All", Name = "GetAllStudents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<IEnumerable<StudentDTO>> GetStudent()
+        {
+            /* var students = new List<StudentDTO>();
+             foreach(var item in CollegeRepository.Students)
+             {
+                 StudentDTO obj = new StudentDTO()
+                 {
+                     Id = item.Id,
+                     StudentName = item.StudentName,
+                     Address = item.Address,
+                     Email = item.Email,
+                 };
+                  students.Add(obj);
+             } */
+            var students = CollegeRepository.Students.Select(s => new StudentDTO()
+            {
+                Id = s.Id,
+                StudentName = s.StudentName,
+                Address = s.Address,
+                Email = s.Email,
+            });
+            // Ok- 200 - Success
+            return Ok(students);
+        }
+
+        [HttpGet]
+        [Route("{id:int}", Name = "GetStudentById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<StudentDTO> GetStudentById(int id)
+        {
+            // BadRequest - 400 - BadRequest - Client Error
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            //NotFound - 404 - NotFound - Client error
+            if (student == null)
+            {
+                return NotFound($"The student with id {id} not found");
+            }
+            var StudentDTO = new StudentDTO()
+            {
+                Id = student.Id,
+                StudentName = student.StudentName,
+                Email = student.Email,
+                Address = student.Address
+            };
+            // Ok - 200 - success
+            return Ok(StudentDTO);
+        }
+
+        [HttpGet]
+        [Route("GetStudentByName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<StudentDTO> GetStudentByName(string name)
+        {
+            //BadRequest - 400 - Badrequest - client error
+            if (string.IsNullOrEmpty(name))
+                return BadRequest();
+
+            var student = CollegeRepository.Students.Where(n => n.StudentName == name).FirstOrDefault();
+            // NotFound - 404 - NotFound - Client error
+            if (student == null)
+                return NotFound($"The student with name {name} not found");
+
+            var StudentDTO = new StudentDTO()
+            {
+                Id = student.Id,
+                StudentName = student.StudentName,
+                Email = student.Email,
+                Address = student.Address
+            };
+
+            // Ok - 200 - Success
+            return Ok(StudentDTO);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        //api/student/create
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<StudentDTO> CreateStudent([FromBody] StudentDTO model)
+        {
+            if (model == null)
+                return BadRequest();
+
+            int newId = CollegeRepository.Students.LastOrDefault().Id + 1;
+            Student student = new Student
+            {
+                Id = newId,
+                StudentName = model.StudentName,
+                Address = model.Address,
+                Email = model.Email,
+            };
+            CollegeRepository.Students.Add(student);
+            model.Id = student.Id;
+            return Ok(model);
+        }
+
+        [HttpDelete("{id}", Name = "DeleteStudentById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult DeleteStudent(int id)
+        {
+            // BadRequest - 400 - BadRequest - Client error
+            if (id <= 0)
+                return BadRequest();
+
+            var student = CollegeRepository.Students.Where(p => p.Id == id).FirstOrDefault();
+            // NotFound - 404 - NotFound - Client error
+            if (student == null)
+                return NotFound($"The student with id {id} not found");
+
+            CollegeRepository.Students.Remove(student);
+            // Ok - 200 - Success
+            return Ok(true);
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        //api/student/update
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult UpdateStudent([FromBody] StudentDTO model)
+        {
+            if (model == null || model.Id <= 0)
+                BadRequest();
+            var existingStudent = CollegeRepository.Students.Where(s => s.Id == model.Id).FirstOrDefault();
+
+            if (existingStudent == null)
+                return NotFound();
+
+            existingStudent.StudentName = model.StudentName;
+            existingStudent.Email = model.Email;
+            existingStudent.Address = model.Address;   
+
+            return NoContent();
+        }
+    }
+}
+
---
+++ CollegeApp1/Models/CollegeRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace CollegeApp1.Models
+{
+    public class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>()
+        {
+            new Student
+             {
+                Id = 1,
+                StudentName = "Afifa",
+                Email = "afifa@gmail.com",
+                Address = "Bengaluru"
+            },
+            new Student
+            {
+                Id = 2,
+                StudentName = "Akshatha",
+                Email = "akshatha@gmail.com",
+                Address = "Bengaluru"
+            },
+            new Student
+            {
+                Id = 3,
+                StudentName = "Jayanth",
+                Email = "jayanth@gmail.com",
+                Address = "Bengaluru"
+            },
+            new Student
+            {
+                Id = 4,
+                StudentName = "Meghana",
+                Email = "meghana@gmail.com",
+                Address = "Bengaluru"
+            },
+             new Student
+            {
+                Id = 5,
+                StudentName = "Spandana",
+                Email = "spandana@gmail.com",
+                Address = "Bengaluru"
+            }
+        }; 
+    }
+}
